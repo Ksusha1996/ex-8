cdd024b467c7cf4008104bb7ce6ff9a42e9d7e68
Axana <kseshechka1996@gmail.com>
2023-08-01 10:28:11 +0300

    tab 8

---
+++ tab 8/Program.cs
@@ -0,0 +1,15 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Напишите программу, которая на вход принимает число (N), а на выходе показывает все четные числа от 1 до N//
+        Console.WriteLine("Введите число N: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine($"Четные числа от 1 до {n}:");
+        for (int i = 2; i <= n; i += 2)
+        {
+
+            Console.WriteLine(i);
+        }
+    }
+}
